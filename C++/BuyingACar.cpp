f3151f130ce801ce2f0348d4b6d5a8e94ba6067c
Kshitiz Chaudhary <54699940+TheKarmaPolice@users.noreply.github.com>
2021-03-15 13:14:26 +0530

    Create BuyingACar.cpp

---
+++ C++/BuyingACar.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+
+class BuyCar
+{
+
+  public:
+  static std::vector<int> nbMonths(int startPriceOld, int startPriceNew, int savingperMonth, double percentLossByMonth)
+  {
+    int count = 0, saving = savingperMonth;
+    double result = startPriceOld - startPriceNew;
+    std::vector<int> answer;
+    answer.reserve(2);
+    
+    double priceOld = static_cast<double>(startPriceOld);
+    double priceNew = static_cast<double>(startPriceNew);
+
+    while (result < 0)
+    {
+      count++;
+      if (count % 2 == 0)
+            percentLossByMonth += 0.5;
+      priceOld -= percentLossByMonth / 100 * priceOld;
+      priceNew -= percentLossByMonth / 100 * priceNew;
+      result = priceOld - priceNew + savingperMonth;   
+      savingperMonth += saving;    
+      
+    }
+    answer.push_back(count);
+    answer.push_back(round(result));
+    return answer;
+    
+  }    
+};
